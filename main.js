b4a0fc4669c18a20173ca37d2c513fafb7066328
veetmoradiya3628 <veetmoradiya3628@gmail.com>
2025-02-08 19:25:06 +0530

    asd

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const Person = {
-    name: 'John Doe',
-    age: 25,
-    greet() {
-        console.log('Hello');
-    }
-}
+  name: "John Doe",
+  age: 25,
+  greet() {
+    console.log("Hello");
+  },
+};
 console.log(Person.name);
